fe02ed2c9467519b9043a0bbbf097fd40bc261ba
Mndub <noureldeen.mohammad@outlook.com>
2023-08-19 16:42:10 +0300

    update

--- Employee_database.py
+++ Employee_database.py
@@ -1,1 +1,14 @@
 #print hello
+#import file
+def convertFileToDictionary(file_pathway):
+  data = {}
+  with open(file_pathway) as f:
+    for line in f:
+      line = line.strip()
+      emp = line.split(',')
+      key = emp[0]
+      value = emp[1:]
+      value[1] = int(value[1])
+      value[3] = int(value[3])
+      data[key] = value
+  return data
